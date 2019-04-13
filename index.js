12451183b1e5d018d8fd2c4c1d1635d519e286d8
Michael Michael <michael.ta.chan@gmail.com>
2019-04-13 08:07:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('div#nested div div div div.target')
+}
+
+function increaseRankBy(n){
+  var list = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i< list.length; i++){
+    list[i].innerHTML = n + parseInt(list[i].innerHTML);
+  }
+}
+
+function deepestChild(){
+  var test = document.querySelector('div#grand-node');
+  var next = test.querySelector('div');
+  
+  while(next){
+    test = next;
+    next = test.querySelector('div');
+  }
+  
+  return test
+}
